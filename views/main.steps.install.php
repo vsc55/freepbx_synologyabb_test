eade183e6752e365244c45d5901bf15ac334f338
Javier Pastor <jpastor@cerebelum.net>
2022-07-31 18:40:34 +0200

    update FAQ install agent

--- views/main.steps.install.php
+++ views/main.steps.install.php
@@ -55,30 +55,56 @@
 
 <h3>F.A.Q.</h3>
 
-<p><b>Problem:</b><br>
-<i>In the installation process the compilation of the kernel module fails, the following error is seen: In kernel headers for this kernel does not seem to be installed.</i></p>
+<h4><b>Problem:</b></h4>
+<p><i>In the installation process the compilation of the kernel module fails, the following error is seen: In kernel headers for this kernel does not seem to be installed.</i></p>
+<br>
+<h4><b>Solution:</b></h4>
+<p>This error has been presented to me in a FreePBX distribution (Sangoma Linux release 7.8.2003 (Core)) and this is the solution that I have found.</p>
+<br>
 
-<p><b>Solution:</b><br>
-This error has been presented to me in a FreePBX distribution (Sangoma Linux release 7.8.2003 (Core)) and this is the solution that I have found.<br>
-<br>
+<h5>Step 1</h5>
 The first thing is to check that we have kernel-devel installed with:<br>
-<i><b># yum install kernel-devel</b></i><br>
-Another way to compare if it is installed is by looking at the directory where the kernels are installed, something like this should come out:<br>
-<i><b># ls -la /usr/src/kernels/<br>
-total 4<br>
-drwxrwxrwx.  3 root root   41 Aug 19  2021 .<br>
-drwxrwxrwx.  8 root root  157 Jul 22 17:35 ..<br>
-drwxr-xr-x  22 root root 4096 Jul 22 17:37 3.10.0-1127.19.1.el7.x86_64</b></i><br>
+&emsp;&emsp;<i><b># yum install kernel-devel</b></i><br>
 <br>
-If all this is correct we will check if the symbolic link of "/lib/modules/{version}/build" exists.<br>
+Another way to compare if it is installed is by looking at the directory where the kernels are installed, something like this should come out:<br>
+&emsp;&emsp;<i><b># ls -la /usr/src/kernels/</b></i><br>
+&emsp;&emsp;<i><b>total 4</b></i><br>
+&emsp;&emsp;<i><b>drwxrwxrwx.  3 root root   41 Aug 19  2021 .</b></i><br>
+&emsp;&emsp;<i><b>drwxrwxrwx.  8 root root  157 Jul 22 17:35 ..</b></i><br>
+&emsp;&emsp;<i><b>drwxr-xr-x  22 root root 4096 Jul 22 17:37 3.10.0-1127.19.1.el7.x86_64</b></i><br>
+<br>
+
+<h5>Step 2</h5>
+If all this is correct we will check if the symbolic link of <b><i>"/lib/modules/{version}/build"</i></b> exists.<br>
 On my system that file did not exist and because of this the kernel module that has to be compiled during the installation process shows the error message.<br>
 We create the symbolic link:<br>
-</i><b># cd /lib/modules/$(uname -r)/<br>
-# ln -s /usr/src/kernels/$(uname -r)/ build</b></i><br>
+&emsp;&emsp;</i><b># cd /lib/modules/$(uname -r)/</b></i><br>
+&emsp;&emsp;</i><b># ln -s /usr/src/kernels/$(uname -r)/ build</b></i><br>
 <br>
-Once we have created this, we start the installation/update process again.</p>
 
+<h5>Step 3</h5>
+Since the module build process failed, trying to reinstall the update will still cause build issues.<br>
+To solve this, before starting the update process we must delete <b><i>"/usr/src/synosnap-{version}/"</i></b>, or do a <b><i>"make clean"</i></b> inside the directory <b><i>"/usr/src/synosnap-{version}/"</i></b>.<br>
+<h5>&emsp;&emsp;Opcion 1:</h5>
+&emsp;&emsp;&emsp;&emsp;</i><b># rm -fr /usr/src/synosnap-*</b></i><br>
+<br>
 
-<hr><br>
+<h5>&emsp;&emsp;Opcion 2:</h5>
+&emsp;&emsp;&emsp;&emsp;</i><b># cd /usr/src/synosnap-{version}/</b></i><br>
+&emsp;&emsp;&emsp;&emsp;</i><b># make clean</b></i><br>
+<br>
 
+<h5>Step 4</h5>
+Once we have created this, we start the installation/update process again.<br>
+<br>
+
+<hr>
+<br>
 <p><b>More info click <a href="https://kb.synology.com/en-global/DSM/help/ActiveBackupBusinessAgent/activebackupbusinessagent?version=7" target="_blank">here</a></b></p>
+
+<script type="text/javascript">
+    $(document).ready(function()
+    {
+        boxLoading(false);
+    });
+</script>
